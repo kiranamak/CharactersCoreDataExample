35cb855c35b848a726f9064895bc4f30e0ee0696
Kiran Mak <kiranamak@gmail.com>
2020-02-03 16:44:53 -0500

    Initial Commit

---
+++ CharactersCoreDataExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CharactersCoreDataExample
+//
+//  Created by Kiran Mak on 2/3/20.
+//  Copyright © 2020 Kiran Mak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
